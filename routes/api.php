8395d51bbdd523c45c3987e54246b3da42f4923a
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2019-07-02 17:12:14 +0300

    chore: Cleanup

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'auth:api'], function () {
-    // @TODO apply auth middleware
     Route::apiResource('hotels', 'HotelController');
     Route::apiResource('room_types', 'RoomTypeController');
     Route::apiResource('rooms', 'RoomController');
